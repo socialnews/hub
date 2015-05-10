4de5c2f1e8832820a4b7c679d4db003cfc332394
geordidearns <geordidearns@gmail.com>
2015-05-10 16:49:37 +1200

    Added Devise omniauth for twitter - Needs refactoring

---
+++ socialnewshub/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  
+  devise_for :users, class_name: 'FormUser', :controllers => { omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations'}
+  root 'welcome#index'
+  
+  get 'welcome/index'
+  resources :users
+end
