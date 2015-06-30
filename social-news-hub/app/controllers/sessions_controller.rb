b03938697eeaf19caff87eb669205617400f8648
geordidearns <geordidearns@carnivallabs.com>
2015-07-01 00:15:46 +1200

    Added sessions controller to store user state once created (signed in) and destroy user state (signed out)

---
+++ social-news-hub/app/controllers/sessions_controller.rb
@@ -0,0 +1,30 @@
+class SessionsController < ApplicationController
+
+	def create
+		auth = request.env["omniauth.auth"] 
+		session[:omniauth] = auth.except('extra')
+		user = User.sign_in_from_omniauth(auth)
+		session[:user_id] = user.id
+		redirect_to root_url, notice: "Signed In"
+	end
+
+	def destroy
+		session[:user_id] = nil
+		session[:omniauth] = nil
+		redirect_to root_url, notice: "Signed Out"
+	end
+end
+
+# Sessions Controller Explained
+
+# create
+
+# auth is assigned the response stored in the enviroment
+# variable you get when you do the callback
+
+# session variable is assigned the response except extra data
+# (twitter hand back alot of unneeded data)
+
+# assign user variable a User object calling
+# the sign_in_from_omniauth method (in models)
+# passing it the response given back from the provider
