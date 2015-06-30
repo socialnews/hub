62b1e56915ff6926f42bbe1c72ddbb366af807de
geordidearns <geordidearns@carnivallabs.com>
2015-07-01 00:26:31 +1200

    add user model methods that find a user from the db to sign them in, or create a new record using provider info

--- social-news-hub/app/models/user.rb
+++ social-news-hub/app/models/user.rb
@@ -1,2 +1,23 @@
 class User < ActiveRecord::Base
+
+	def self.sign_in_from_omniauth(auth)
+		find_by(provider: auth['provider'], uid: auth['uid']) || create_user_from_omniauth(auth)
+	end
+
+	def self.create_user_from_omniauth(auth)
+		create(
+			provider: auth['provider'],
+			uid: auth['uid'],
+			name: auth['info']['name']
+		)
+	end
 end
+
+# User Model explained
+
+# call a self method on the User object that 
+# finds a user from the response given from the provider (the auth paramter passed in)
+# OR call create_user_from_omniauth if they do not exist
+
+# create_user_from_omniauth takes the response from
+# the provider and uses the necessary info to create a new record
